0384a84c0b3dd671ffd99839fde80a20991dcab5
ocknamo <s14pes@gmail.com>
2023-10-08 02:00:44 +0900

    feat: create shared package repo

---
+++ package/shared/cryoto/random.ts
@@ -0,0 +1,27 @@
+/**
+ * Get random base64url.
+ * @param digit number
+ * @returns base64url string
+ */
+export function getRandomBase64Url(digit: number = 12): string {
+  const arrayView = new Uint8Array(digit);
+  crypto.getRandomValues(arrayView);
+
+  const str = String.fromCharCode.apply(
+    null,
+    Array.from((crypto.getRandomValues(arrayView))),
+  );
+  const base64 = btoa(str);
+  const base64url = base642base64url(base64);
+
+  return base64url.slice(0, digit);
+}
+
+/**
+ * Convert to base64 to base64url.
+ * @param base64
+ * @returns base64url string
+ */
+function base642base64url(base64: string): string {
+  return base64.replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+}
