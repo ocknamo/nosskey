f38290ad4d1c4bc30420d19fba687cafa22cb0d3
ocknamo <s14pes@gmail.com>
2023-10-25 23:35:27 +0900

    feat: update strage class

--- package/web-client/src/lib/storage/strage.spec.ts
+++ package/web-client/src/lib/storage/strage.spec.ts
@@ -6,17 +6,57 @@ describe('Strage', () => {
 
 	beforeEach(() => {
 		service = new Strage();
+
+		window.sessionStorage.clear();
+		window.localStorage.clear();
 	});
 
 	it('should be exist', () => {
 		expect(service).toBeDefined();
 	});
 
-	it('should set and get session strage', async () => {
+	it('should set and get EncryptionKey', async () => {
 		await service.setEncryptionKey('enc_key');
 
 		const res = service.getEncryptionKey();
 
 		expect(res).toBe('enc_key');
+	});
+
+	it('should set and get UserName', async () => {
+		await service.setUserName('user_name');
+
+		const res = service.getUserName();
+
+		expect(res).toBe('user_name');
+	});
+
+	it('should set and get Npub', async () => {
+		await service.setNpub('n_public_key');
+
+		const res = service.getNpub();
+
+		expect(res).toBe('n_public_key');
+	});
+
+	it('should set and get Nsec', async () => {
+		await service.setNsec('n_secret_key');
+
+		const res = service.getNsec();
+
+		expect(res).toBe('n_secret_key');
+	});
+
+	it('should clear all', async () => {
+		await service.setUserName('user_name');
+
+		let res = service.getUserName();
+
+		expect(res).toBe('user_name');
+
+		service.clearAll();
+
+		res = service.getUserName();
+		expect(res).toBe(null);
 	});
 });
--- package/web-client/src/lib/storage/strage.ts
+++ package/web-client/src/lib/storage/strage.ts
@@ -2,6 +2,9 @@
  * Strage keys
  */
 const enkKeyKey = 'encryption_key';
+const userNameKey = 'user_name';
+const npubKey = 'npublic_key';
+const nsecKey = 'nsecret_key';
 
 export class Strage {
 	setEncryptionKey(enkKey: string): void {
@@ -10,6 +13,35 @@ export class Strage {
 
 	getEncryptionKey(): string | null {
 		return this.getFromSession(enkKeyKey);
+	}
+
+	setUserName(userName: string): void {
+		this.setToLocal(userNameKey, userName);
+	}
+
+	getUserName(): string | null {
+		return this.getFromLocal(userNameKey);
+	}
+
+	setNpub(npub: string): void {
+		this.setToLocal(npubKey, npub);
+	}
+
+	getNpub(): string | null {
+		return this.getFromLocal(npubKey);
+	}
+
+	setNsec(nsec: string): void {
+		this.setToSession(nsecKey, nsec);
+	}
+
+	getNsec(): string | null {
+		return this.getFromSession(nsecKey);
+	}
+
+	clearAll(): void {
+		window.sessionStorage.clear();
+		window.localStorage.clear();
 	}
 
 	private setToSession(key: string, value: string): void {
